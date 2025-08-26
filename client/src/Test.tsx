afec8d846fc1ecc31a3421e84df0fba0576dd9e0
Margot Bihan <mbihan@1B6.42angouleme.fr>
2025-08-26 09:26:41 +0200

    Feat: HTTP request

--- client/src/Test.tsx
+++ client/src/Test.tsx
@@ -1,11 +1,22 @@
+import {useEffect, useState} from "react";
 
 
 function Test() {
+
+    const [message, setMessage] = useState<string>('');
+
+    useEffect(() =>{
+        fetch("http://localhost:3000/test")
+            .then((res) => res.text())
+            .then((data) => setMessage(data))
+    })
+
 
   return (
     <>
       <h1>Voici un test du router</h1>
       <p>Ça marche bien !</p>
+        <p>message = {message}</p>
     </>
   )
 
--- server/dist/index.js
+++ server/dist/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import cors from "cors";
 const app = express();
 const PORT = 3000;
+app.use(cors());
+app.get('/test', (req, res) => {
+    res.send('test recu');
+});
 app.listen(PORT, () => {
     console.log(`Port = ${PORT}`);
 });
