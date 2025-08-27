84129dd5cf965591c77f378bda73ed1af651ff91
Margot Bihan <mbihan@1B5.42angouleme.fr>
2025-08-27 10:14:17 +0200

    Corr enum

--- client/src/Components/GameLobby.tsx
+++ client/src/Components/GameLobby.tsx
@@ -1,20 +1,16 @@
 import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 
-enum StatusState {
-    Error,
-    Waiting,
-    Game
-}
+// import WaitingRoom from "./WaitingRoom.ts";
+
+type StatusState = "Error" | "Waiting" | "Game";
+
 
 function UrlErrorCheck(room: string | undefined, login: string | undefined): string | null {
     if (room === undefined || login === undefined) {
         return "Room or login missing in the url";
     }
     if (isNaN(Number(room)) || !Number.isInteger(Number(room))) {
-        return "Invalid room. Must be a number between 1 and 100";
-    }
-    if (Number(room) <= 0 || Number(room) > 100) {
         return "Invalid room. Must be a number between 1 and 100";
     }
     if (login.length < 3 || login.length > 20) {
@@ -27,18 +23,20 @@ export default function GameLobby() {
     const {room, login} = useParams<{ room: string, login: string }>()
     const [status, setStatus] = useState<StatusState>()
     const [errorMessage, setErrorMessage] = useState<string>("")
+    // const [leader, setLeader] = useState(false)
 
 
     useEffect(() => {
         const urlError = UrlErrorCheck(room, login)
         if (urlError) {
             setErrorMessage(urlError)
-            setStatus(StatusState.Error)
-        }
+            setStatus("Error")
+        } else
+            setStatus("Waiting")
     }, [room, login]);
 
 
-    if (status === StatusState.Error) {
+    if (status === "Error") {
         return (
             <>
                 <p>Error on the URL : {errorMessage}</p>
@@ -46,11 +44,18 @@ export default function GameLobby() {
                 <p>http://localhost:5173/room/player_name</p>
             </>
         )
-    } else
-        return (
-            <>
-                <p>Hello {login}</p>
-                <p>You joined Room {room}</p>
-            </>
-        )
+    }
+    // } else if (status === StatusState.Waiting && room && login) {
+    //     return (
+    //         <>
+    //             <WaitingRoom room={room} login={login} leader={leader}/>
+    //         </>
+    //     )
+    // }
+    return (
+        <>
+            <p>Hello {login}</p>
+            <p>You joined Room {room}</p>
+        </>
+    )
 }
---
+++ client/src/Components/WaitingRoom.ts
@@ -0,0 +1,13 @@
+// import {useState} from "react";
+//
+// interface WaitingRoomProps {
+//     room: string
+//     login: string
+//     leader: boolean
+// }
+//
+// export default function WaitingRoom({room, login, leader}: WaitingRoomProps) {
+//     const [listPlayers, setListPlayers] = useState([]);
+//
+//
+// }
