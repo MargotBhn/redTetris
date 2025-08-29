92169e98f38b55625fd906391bcea97bfa8f05d3
Folaste <41912691+Folaste@users.noreply.github.com>
2025-08-29 11:34:27 +0200

    Adding Home to have an entry page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import {BrowserRouter, Route, Routes} from 'react-router'
 import './App.css'
 import Test from './Test'
 import GameLobby from "./Components/GameLobby.tsx";
+import Home from './Home'
 
 function App() {
-
     return (
         <BrowserRouter>
             <Routes>
+                <Route path={'/'} element={<Home />} />
                 <Route path={'/test'} element={<Test/>}/>
                 <Route path={'/:room/:login'} element={< GameLobby/>}/>
             </Routes>
---
+++ client/src/Home.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Home() {
+    return (
+        <>
+            <p className="text-xl">Bienvenue sur RedTetris !</p>
+            <p>Pour jouer, merci de ajouter à l'URL : "/roomName/user_name"</p>
+        </>
+    )
+}
